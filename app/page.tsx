2c2f8c411c2fc4776f5ea9cd93e44cb010aba37b
Jesse Masika <jessemasika002@gmail.com>
2026-05-09 12:42:47 +0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+       <div className="flex justify-center items-center h-screen">
+           <button>click me</button>
+
+       </div>
+    )
+}
+export default Page
+
