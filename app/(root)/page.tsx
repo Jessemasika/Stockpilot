b7955d2ac4691b88aac9874021f7c2c2f6e3cf54
Jesse Masika <jessemasika002@gmail.com>
2026-05-11 16:30:15 +0300

    impliment header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+       <div className="flex min-h-screen" home-wrapper>
+        home
+       </div>
+    )
+}
+export default Page
+
